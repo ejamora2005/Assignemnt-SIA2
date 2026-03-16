27b098fe851af21422490f872e3ff9ff42e4bb56
Emil Jon Amora <ejamora19@gmail.com>
2026-03-17 07:55:43 +0800

    Initial upload of SIA2 assignment

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.admin')
+
+@section('title', 'Admin Dashboard')
+
+@section('content')
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            <div class="card shadow-sm border-0">
+                <div class="card-body p-4">
+                    <h1 class="h3 mb-3">Admin Dashboard</h1>
+                    <p class="mb-0 text-secondary">This is a simple admin page to test your admin section.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
